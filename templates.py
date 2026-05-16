fac65012abf20a30b5489529eee66e039b978268
Kani201012 <kiran@kaydiemscriptlab.com>
2026-05-16 10:02:10 +0300

    Fix script formatting in templates.py

--- templates.py
+++ templates.py
@@ -354,7 +354,6 @@ def gen_nav(cfg: 'SiteConfig') -> str:
     const nav = document.getElementById('main-navbar');
     if (topBar && nav) {{
         nav.style.top = topBar.offsetHeight + 'px';
-    }}
 }})();
 </script>"""
 
